4fc82c30219595b5a1a5fd25ae12843453f7fc1b
Fronsto <an.vijay.q@gmail.com>
2024-04-18 00:05:54 +0530

    Added support for verifying aggregate signature for distinct messages signed by distinct entities

--- tests/test_bls.py
+++ tests/test_bls.py
@@ -42,3 +42,19 @@ def test_multi_authorities():
 
 	# verify signature
 	assert verify(params, aggr_vk, sigma, m)
+
+def test_distinct_messages_with_multi_authorities():
+	m = [10,20,30,40,50]
+	n = 5 # number of authorities
+	params = setup()
+
+	# generate key
+	(sk, vk) = ttp_keygen(params, n, n)
+
+	# sign
+	sigs = [sign(params, ski, [mi]) for ski,mi in zip(sk,m)]
+
+	# aggregate credentials
+	sigma = aggregate_sigma(params, sigs, threshold=False)
+
+	assert aggregate_verify(params, vk, sigma, m)
